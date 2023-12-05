98831dcc4021dc1a2519a7c5431c749e905d0c0f
Ray <675983230@qq.com>
2023-12-05 13:41:58 +0800

    ver openlayer

---
+++ main.js
@@ -0,0 +1,98 @@
+import Feature from 'ol/Feature.js';
+import Geolocation from 'ol/Geolocation.js';
+import Map from 'ol/Map.js';
+import Point from 'ol/geom/Point.js';
+import View from 'ol/View.js';
+import { Circle as CircleStyle, Fill, Stroke, Style } from 'ol/style.js';
+import { OSM, Vector as VectorSource } from 'ol/source.js';
+import { Tile as TileLayer, Vector as VectorLayer } from 'ol/layer.js';
+
+const view = new View({
+  projection: "EPSG:4326",
+  center: [115.796513, 28.658448],
+  zoom: 17,
+});
+
+const map = new Map({
+  layers: [
+    new TileLayer({
+      source: new OSM(),
+    }),
+  ],
+  target: 'map',
+  view: view,
+});
+
+const geolocation = new Geolocation({
+  // enableHighAccuracy must be set to true to have the heading value.
+  trackingOptions: {
+    enableHighAccuracy: true,
+  },
+  projection: view.getProjection(),
+});
+
+function el(id) {
+  return document.getElementById(id);
+}
+
+el('track').addEventListener('change', function () {
+  geolocation.setTracking(this.checked);
+});
+
+// update the HTML page when the position changes.
+geolocation.on('change', function () {
+  el('accuracy').innerText = geolocation.getAccuracy() + ' [m]';
+  el('altitude').innerText = geolocation.getAltitude() + ' [m]';
+  el('altitudeAccuracy').innerText = geolocation.getAltitudeAccuracy() + ' [m]';
+  el('heading').innerText = geolocation.getHeading() + ' [rad]';
+  el('speed').innerText = geolocation.getSpeed() + ' [m/s]';
+});
+
+// handle geolocation error.
+geolocation.on('error', function (error) {
+  const info = document.getElementById('info');
+  info.innerHTML = error.message;
+  info.style.display = '';
+});
+
+const accuracyFeature = new Feature();
+geolocation.on('change:accuracyGeometry', function () {
+  accuracyFeature.setGeometry(geolocation.getAccuracyGeometry());
+});
+
+const positionFeature = new Feature();
+positionFeature.setStyle(
+  new Style({
+    image: new CircleStyle({
+      radius: 6,
+      fill: new Fill({
+        color: '#3399CC',
+      }),
+      stroke: new Stroke({
+        color: '#fff',
+        width: 2,
+      }),
+    }),
+  })
+);
+function addOffsetToCoordinate(coordinate, offset) {
+  return [coordinate[0] + offset[0], coordinate[1] + offset[1]];
+}
+geolocation.on('change:position', function () {
+  const originalCoordinates = geolocation.getPosition();
+  console.log(originalCoordinates);
+  const offset = [0.0081470779, 0.00083344287929]; // Adjust the offset as needed
+  const offsetCoordinates = addOffsetToCoordinate(originalCoordinates, offset);
+  positionFeature.setGeometry(offsetCoordinates ? new Point(offsetCoordinates) : null);
+});
+
+new VectorLayer({
+  map: map,
+  source: new VectorSource({
+    features: [accuracyFeature, positionFeature],
+  }),
+});
+//单击获取地图坐标
+map.on('singleclick', (evt) => {
+  console.log(evt.coordinate);
+});
