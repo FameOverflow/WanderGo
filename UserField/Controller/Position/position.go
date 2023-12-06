f6ab11c21b3b7cc59acb4a5f683c35fe23fb7199
Fl0rencess720 <2391793239@qq.com>
2023-12-06 20:17:33 +0800

    将漫游功能的米计算换成了经纬度计算

--- UserField/Controller/Position/position.go
+++ UserField/Controller/Position/position.go
@@ -44,7 +44,7 @@ func PositionHandlerComment(a con.Address) con.Address {
 func Roaming(ctx *gin.Context) {
 	centerPoint := PositionHandler(ctx)
 	var places []con.Place
-	con.GLOBAL_DB.Model(&con.Place{}).Where("POWER(? - JSON_EXTRACT(center_point, '$.x'), 2) + POWER(? - JSON_EXTRACT(center_point, '$.y'), 2) <= 160000", centerPoint.X, centerPoint.Y).Find(&places)
+	con.GLOBAL_DB.Model(&con.Place{}).Where("(POWER(? - JSON_EXTRACT(center_point, '$.x'), 2) / POWER(0.0035972, 2)) + (POWER(? - JSON_EXTRACT(center_point, '$.y'), 2) / POWER(0.0045, 2)) <= 1", centerPoint.X, centerPoint.Y).Find(&places)
 	randomIndex := rand.Intn(len(places))
 	selectedPlace := places[randomIndex]
 	var pl con.Place
